11c16aebf37b6de277c7911b4c068cf60b9b11c7
Abraham Pabbathi <apabbathi@smartdataanalytics.solutions>
2024-01-26 14:48:12 +0000

    Changes from context manager

--- 01-create-tables.py
+++ 01-create-tables.py
@@ -4,5 +4,5 @@
 
 # COMMAND ----------
 
-df = spark.read.option("header","true").csv("/mnt/raw/uploadpickup.csv")
+df = spark.read.option("header","true").csv(get_uc_mount_target('/mnt/raw', normalize=True) + "/uploadpickup.csv")
 display(df)
